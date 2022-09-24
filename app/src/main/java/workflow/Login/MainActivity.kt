d3c271625a076e1642b734d0b7fa95cf08771297
Kaplenkov_Aleksandr <kaplenkovsasha03@gmail.com>
2022-09-24 14:50:32 +0300

    Done Hibru and button implementation

--- app/src/main/java/workflow/Login/MainActivity.kt
+++ app/src/main/java/workflow/Login/MainActivity.kt
@@ -1,11 +1,56 @@
 package workflow.Login
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.res.Configuration.*
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<EditText>(R.id.Password).setOnEditorActionListener { _, id, _ ->
+            when (id) {
+                EditorInfo.IME_ACTION_DONE -> {
+                    LogInButton.performClick()
+                    true
+                }
+                else -> {
+                    false
+                }
+            }
+        }
     }
+
+    fun btnPressed(view: View) {
+        when (view.id) {
+
+            R.id.LogInButton -> {
+                if (Password.text.toString().isEmpty()) {
+                    alertBox.text = getString(R.string.PasswordAlert)
+                } else if (Login.text.toString().isEmpty()) {
+                    alertBox.text = getString(R.string.LoginAlert)
+                } else {
+                    alertBox.text = getString(R.string.Alert)
+                }
+            }
+
+            R.id.changeThemeButton -> {
+                when (resources.configuration.uiMode and UI_MODE_NIGHT_MASK) {
+                    UI_MODE_NIGHT_NO -> {
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                    }
+                    UI_MODE_NIGHT_YES -> {
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                    }
+                }
+            }
+        }
+    }
+
 }
